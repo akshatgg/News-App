23fca93a94923992bcb387d187929318f1edb08c
akshatgg <akshatg9636@gmail.com>
2023-12-02 03:20:55 +0545

    changing the position of router

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -10,22 +12,23 @@ const router = (
   <Router>
     <Routes>
       <Route path="/" element={<App />}>
-        <Route path="" element={<News key="general" apikey={apikey} pagesize={6}  country="in" categories="general"/>} >
-        <Route path="au" element={<News key="generals" apikey={apikey} pagesize={6} country="au" categories="general" />}>
-          <Route path="/au/health" element={<News key="health" apikey={apikey} pagesize={6} categories="health" country="au" />} />
-       </Route>
-       </Route>
+        <Route
+          path="au"
+          element={
+            <News key="general" apikey={apikey} pagesize={6} country="au" categories="general" />
+          }
+        >
+          <Route path="health" element={<News key="health" apikey={apikey} pagesize={6} categories="health" country="au" />} />
+          
+       
+       
+       
+       
         </Route>
-        {/* 
-        <Route path="as" element={<News key="general" apikey={apikey} pagesize={6} categories="general" country="as" />} />
-        <Route path="at" element={<News key="general" apikey={apikey} pagesize={6} categories="general" country="at" />} />
-        <Route path="aw" element={<News key="general" apikey={apikey} pagesize={6} categories="general" country="au" />} />
-        */}
-      
+      </Route>
     </Routes>
   </Router>
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode>{router}</React.StrictMode>);
-
